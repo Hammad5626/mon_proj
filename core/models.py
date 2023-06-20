23cbb5325502b3eefc1713f9ecb69f584a099b4a
Hamad Badar <hamadbadar5626@hotmail.com>
2023-06-20 15:30:34 +0500

    Minor Fixes in User_Data

--- core/models.py
+++ core/models.py
@@ -9,18 +9,21 @@ class UserModel(models.Model):
     
 class DataModel(models.Model):
     class Meta:
-        ordering = ('opening_time',)
+        ordering = ('time',)
         
     user = models.ForeignKey(UserModel, on_delete=models.CASCADE)
-    opening_time = models.DateTimeField()
+    time = models.DateTimeField()
     type = models.CharField(max_length=100)
     volume = models.FloatField(null=True)
     symbol = models.CharField(max_length=100, null=True)
     opening_price = models.FloatField(null=True)
-    volumn_spent = models.FloatField(null=True)
+    volume_spent = models.FloatField(null=True)
     closing_time = models.DateTimeField(null=True)
     price = models.FloatField(null=True)
+    commision = models.FloatField(null=True)
+    swap = models.FloatField(null=True)
     profit = models.FloatField(null=True)
+    net_profit = models.FloatField(null=True)
 
     def __str__(self):
         return f"{self.opening_time} | {self.symbol} | {self.type}"
